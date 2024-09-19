40427e9fb4499f416db1cfa8b5d98e81bd5c9cd6
Mariana <163425141+marianalmeid@users.noreply.github.com>
2024-09-19 11:37:53 -0300

    aula1909

---
+++ aula19-09/repeticao.js
@@ -0,0 +1,12 @@
+console.log("utfpr")
+console.log("utfpr")
+console.log("utfpr")
+console.log("utfpr")
+console.log("utfpr")
+
+let contador = 1
+while(contador <= 5){
+    console.log(contador, "utfpr")
+
+    contador = contador + 1
+}
